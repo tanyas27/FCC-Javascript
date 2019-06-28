b15db78f813f22b0d7c5187ed7df4f5b323a2a3a
Tanya Singh <tanyacs27@gmail.com>
2019-06-28 21:12:09 +0530

    Edited challenge 01

--- 01-Basics/js01.js
+++ 01-Basics/js01.js
@@ -6,7 +6,7 @@ There are two ways to write comments in JavaScript:
 Using // will tell JavaScript to ignore the remainder of the text on the current line:
 
 // This is an in-line comment.
-You can make a multi-line comment beginning with /* and ending with */:
+You can make a multi-line comment beginning with /* and ending with */
 
 /* This is a
 multi-line comment */
