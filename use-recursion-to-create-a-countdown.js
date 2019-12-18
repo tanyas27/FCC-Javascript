b76c5fbc35ffe84fb80e62a87e323727469fe82d
tanyasingh27 <52325969+tanyasingh27@users.noreply.github.com>
2019-12-18 23:59:16 +0530

    Add files via upload

---
+++ use-recursion-to-create-a-countdown.js
@@ -0,0 +1,14 @@
+
+function countdown(n){
+  if(n<1)
+  {
+   return [];
+  }
+  else
+  {
+  const arr = countdown(n-1);
+  arr.unshift(n);
+  return arr;
+  }
+}
+console.log(countdown(5)); // [5, 4, 3, 2, 1]
